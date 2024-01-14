c2299a3976ac134359feb5a63eedb4588feaf0d4
pharaok <farouk.tamer03@gmail.com>
2024-01-15 00:29:03 +0200

    setup socketio server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,13 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const server = createServer();
+const io = new Server(server, { cors: { origin: "http://localhost:3000" } });
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3001, () => {
+  console.log("listening on 3001");
+});
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { io, Socket } from "socket.io-client";
+
 export default function Page() {
+  const socket = useRef<Socket<{}, {}> | null>();
+  useEffect(() => {
+    socket.current = io("http://localhost:3001");
+    console.log("in here");
+  }, []);
+
   return <main>hello world</main>;
 }
